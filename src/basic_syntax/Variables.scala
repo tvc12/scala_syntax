30c56cebb97723ab0420335bf0f96d1f0280f32b
tvc12 - Vi Chi Thien <meomeocf98@gmail.com>
2019-11-11 14:16:20 +0700

    Add variables

---
+++ src/basic_syntax/Variables.scala
@@ -0,0 +1,7 @@
+println("Variable are like values, can re-assign them, define by 'var' keyword")
+var x = 1 + 2
+println(s"Value is x: $x")
+
+var defaultValue: Int = 4
+defaultValue = 1 + 2
+println(s"default value $defaultValue")
