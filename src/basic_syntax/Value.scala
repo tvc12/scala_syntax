2ab34c5f56aa0ab87b43012934d69174ec057a63
tvc12 - Vi Chi Thien <meomeocf98@gmail.com>
2019-11-11 14:09:41 +0700

    Add values syntax scala

---
+++ src/basic_syntax/Value.scala
@@ -0,0 +1,7 @@
+val valueInt = 1 + 1
+println(s"Value int: $valueInt")
+val valueString = "1 + 1"
+println(s"Value is String $valueString");
+
+val a : Int = 1 + 1
+println(s"Set type is a int: $a")
